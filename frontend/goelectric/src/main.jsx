48bea35cd430776b1c3d5379e270eb6e14b7cd7e
Jakub Kowieski <01158762@pw.edu.pl>
2022-05-05 19:52:59 +0200

    Updated front

--- frontend/goelectric/src/main.jsx
+++ frontend/goelectric/src/main.jsx
@@ -9,8 +9,6 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
-import Homepage from "./pages/Homepage";
-import TinderPage from "./pages/TinderPage";
 import Compare from "./pages/Compare";
 
 ReactDOMClient.createRoot(document.getElementById("root")).render(
@@ -19,7 +17,6 @@ ReactDOMClient.createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<Homepage />} />
-          <Route path="tinder" element={<TinderPage />} />
           <Route path="compare" element={<Compare />} />
           <Route path="about" element={<AboutPage />} />
         </Route>
